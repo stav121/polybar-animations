85b484e6c5c23ff02a5befe27824c8ed283278fa
unix121@protonmail.com <unix121@protonmail.com>
2018-02-07 16:10:24 +0200

    First commit

---
+++ space-clock/space-clock.py
@@ -0,0 +1,26 @@
+#!/bin/env python
+
+import re
+import time
+
+if __name__=="__main__" :
+
+    max = 275
+
+    with open( ".spaceclock.txt", 'r+' ) as file:
+        line = file.read( )
+        line = line.strip( )
+
+        output = int(line)*' ' + '[' + time.strftime("%X") + ']>- '
+        print( output )
+        int_line = int( line )
+        int_line = int_line+1
+
+        if int_line > max :
+            int_line = 0
+
+        file.seek( 0 )
+        file.write( str( int_line ) )
+        file.truncate( )
+        file.close( )
+
